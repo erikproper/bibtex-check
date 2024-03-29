a9327e4bb912d54c649e9b3c29825ac028e4d928
erikproper <erikproper@gmail.com>
2024-03-29 15:34:29 +0100

    $1

--- main.go
+++ main.go
@@ -5,90 +5,14 @@ import "os"
 
 //import "io"
 import "bufio"
+import "bytes"
 
 //import "strconv"
 
-/////// PASCALIFY parameters
-
-func IsEven(Number int) bool {
-	return Number%2 == 0
-}
-
-//
-// Sets of named strings
-//
-
-type TNamedStringsSet map[string]string
-
-func (Set TNamedStringsSet) AddRange(Items []string) TNamedStringsSet {
-	// Takes a set of strings as input.
-	// This set should actually be pairs of strings and their names.
-	// As such, it should always be an even range
-	var s string
-	var l int
-
-	for i, Item := range Items {
-		l = i
-
-		if IsEven(i) {
-			s = Item
-		} else {
-			Set[s] = Item
-		}
-	}
-
-	// We should never have this situation ... can we throw an exception and bring down the app?
-	if IsEven(l) {
-		fmt.Println("ERROR: We are missing a name for a range of named strings: ")
-		fmt.Println(" - Set  :", Set)
-		fmt.Println(" - Items:", Items)
-	}
-
-	return Set
-}
-
-func NewNamedStringsSet(Items ...string) TNamedStringsSet {
-	Set := TNamedStringsSet{}
-
-	return Set.AddRange(Items)
-}
-
-func (Set TNamedStringsSet) Add(Items ...string) TNamedStringsSet {
-	return Set.AddRange(Items)
-}
-
-func (Set TNamedStringsSet) Remove(Items ...string) TNamedStringsSet {
-	for _, Item := range Items {
-		delete(Set, Item)
-	}
-
-	return Set
-}
-
-func (Set TNamedStringsSet) Contains(Items ...string) bool {
-	Contains := true
-	ContainsItems := true
-
-	for _, Item := range Items {
-		_, Contains = Set[Item]
-
-		ContainsItems = ContainsItems && Contains
-
-		// We are done as soon as we come across an Item that is not contained in the set
-		if !ContainsItems {
-			return ContainsItems
-		}
-	}
-
-	return ContainsItems
-}
-
-//////
-
 type TLexerControl struct {
-	Escapers   TNamedStringsSet
-	Delimiters TNamedStringsSet
-	Singletons TNamedStringsSet
+	Escapers   TByteSet
+	Delimiters TByteSet
+	Singletons TByteSet
 }
 
 type Text struct {
@@ -148,43 +72,192 @@ func (t *Text) parseCommentsEty() bool {
 
 func (t *Text) NextToken() bool {
 	result :=
-		t.NextChar() &&
-			t.parseCommentsEty()
+		t.NextChar() //&&
+		//t.parseCommentsEty()
 
 	return result
 }
 
 ///////////////////
 
+type TByteSet struct {
+	words        [4]uint64
+	treatAsChar bool
+}
+
+func (s *TByteSet) split(b byte) (byte, uint64) {
+	return b / 64, 1 << byte(b%64)
+}
+
+func ByteSet(elements ...byte) TByteSet {
+	var newSet TByteSet
+
+	newSet.add(elements)
+
+	return newSet
+}
+
+func (s *TByteSet) TreatAsChar() *TByteSet {
+	s.treatAsChar = true
+
+	return s
+}
+
+func (s *TByteSet) add(elements []byte) *TByteSet {
+	for _, element := range elements {
+		word, bit := s.split(element)
+
+		s.words[word] += bit
+	}
+
+	return s
+}
+
+func (s *TByteSet) Add(elements ...byte) *TByteSet {
+	return s.add(elements)
+}
+
+func (s *TByteSet) delete(elements []byte) *TByteSet {
+	for _, element := range elements {
+		word, bit := s.split(element)
+
+		s.words[word] &^= bit
+	}
+
+	return s
+}
+
+func (s *TByteSet) Delete(elements ...byte) *TByteSet {
+	return s.delete(elements)
+}
+
+func (s *TByteSet) Unite(t TByteSet) *TByteSet {
+	s.words[0] |= t.words[0]
+	s.words[1] |= t.words[1]
+	s.words[2] |= t.words[2]
+	s.words[3] |= t.words[3]
+
+	return s
+}
+
+func (s *TByteSet) Intersect(t TByteSet) *TByteSet {
+	s.words[0] &= t.words[0]
+	s.words[1] &= t.words[1]
+	s.words[2] &= t.words[2]
+	s.words[3] &= t.words[3]
+
+	return s
+}
+
+func (s *TByteSet) Subtract(t TByteSet) *TByteSet {
+	s.words[0] &^= t.words[0]
+	s.words[1] &^= t.words[1]
+	s.words[2] &^= t.words[2]
+	s.words[3] &^= t.words[3]
+
+	return s
+}
+
+func (s TByteSet) Eq(t TByteSet) bool {
+	return s.words[0] == t.words[0] && s.words[1] == t.words[1] && s.words[2] == t.words[2] && s.words[3] == t.words[3]
+}
+
+func (s TByteSet) SubsetEq(t TByteSet) bool {
+	return s.words[0]&^t.words[0] == 0 && (s.words[1]&^t.words[1] == 0) && (s.words[2]&^t.words[2] == 0) && (s.words[3]&^t.words[3] == 0)
+}
+
+func (s TByteSet) Subset(t TByteSet) bool {
+	return s.SubsetEq(t) && !s.Eq(t)
+}
+
+func (s TByteSet) SupersetEq(t TByteSet) bool {
+	return t.SubsetEq(s)
+}
+
+func (s TByteSet) Superset(t TByteSet) bool {
+	return s.SupersetEq(t) && !s.Eq(t)
+}
+
+func (s TByteSet) Contains(elements ...byte) bool {
+	var elementSet TByteSet
+
+	elementSet.add(elements)
+
+	return true
+}
+
+
+var ByteToString [255]string
+
+func (s TByteSet) String() string {
+	var buf bytes.Buffer
+	var item byte
+
+	fmt.Fprint(&buf, "{")
+
+	for w := 0; w < 4; w++ {
+		for b := 0; b < 64; b++ {
+			if (s.words[w] & (1 << b)) > 0 {
+				if buf.Len() > 1 {
+					fmt.Fprint(&buf, " ,")
+				}
+
+				item = byte(w*64 + b)
+				if s.treatAsChar {
+					fmt.Fprint(&buf, "'", ByteToString[item], "'")
+				} else {
+					fmt.Fprintf(&buf, "%d", item)
+				}
+			}
+		}
+	}
+
+	fmt.Fprint(&buf, "}")
+
+	return buf.String()
+}
+
+func init() {
+	for i := 0; i < 255; i++ {
+		ByteToString[i] = string(i)
+	}
+	ByteToString['\\'] = "\\\\"
+	ByteToString['\a'] = "\\a"
+	ByteToString['\b'] = "\\b"
+	ByteToString['\f'] = "\\f"
+	ByteToString['\n'] = "\\n"
+	ByteToString['\r'] = "\\r"
+	ByteToString['\t'] = "\\t"
+	ByteToString['\v'] = "\\v"
+}
+
+var MainStructureLexerControl, QuotedFieldLexerControl, BracketedFieldLexerControl TLexerControl
+
 func main() {
-	var LexerControl TLexerControl
-	//	var bibTeX Text
-	//	var count int
+	MainStructureLexerControl.Escapers.Add().TreatAsChar()
+	MainStructureLexerControl.Delimiters.Add(' ', '\n', '\t', '\a', '\b', '\f', '\r', '\v').TreatAsChar()
+	MainStructureLexerControl.Singletons.Add('@', '%', '{', '}', '#', '"').TreatAsChar()
 
-	LexerControl.Escapers = NewNamedStringsSet(
-		"\\", "slash")
+	BracketedFieldLexerControl.Escapers.Add('\\').TreatAsChar()
+	BracketedFieldLexerControl.Delimiters.Add(' ', '\n', '\t', '\a', '\b', '\f', '\r', '\v').TreatAsChar()
+	BracketedFieldLexerControl.Singletons.Add('{', '}', '\\').TreatAsChar()
 
-	LexerControl.Delimiters = NewNamedStringsSet(
-		" ", "space")
+	QuotedFieldLexerControl.Escapers.Add('\\').TreatAsChar()
+	QuotedFieldLexerControl.Delimiters.Add(' ', '\n', '\t', '\a', '\b', '\f', '\r', '\v').TreatAsChar()
+	QuotedFieldLexerControl.Singletons.Add('{', '}', '\\', '"').TreatAsChar()
 
-	LexerControl.Singletons = NewNamedStringsSet(
-		"@", "at",
-		"%", "percent",
-		"{", "left curly bracket",
-		"}", "right curly bracket",
-		"\"", "double quote")
-
-	fmt.Println(LexerControl)
+	fmt.Println(MainStructureLexerControl)
+	fmt.Println(BracketedFieldLexerControl)
+	fmt.Println(QuotedFieldLexerControl)
 
 	//	bibTeX.Escape = '\\'
 	//	bibTeX.CommentStart = '%'
 	//	bibTeX.CommentEnd = '\n'
 
 	//	if bibTeX.TextFileOpen("Test.bib") {
-	//		for bibTeX.NextToken() {
-	//			count++
-	//			fmt.Printf("[" + string(bibTeX.ThisChar()) + "]")
-	//		}
+	//			for bibTeX.NextToken() {
+	//				fmt.Printf("[" + string(bibTeX.ThisChar()) + "]")
+	//			}
 	//	}
 	//
 	// fmt.Printf("\n")
