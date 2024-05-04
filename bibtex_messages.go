e313d0f89dc4bb7cbb8396b71ccaaf6ef54af6ae
erikproper <erikproper@gmail.com>
2024-05-04 14:03:21 +0200

    $1

---
+++ bibtex_messages.go
@@ -0,0 +1,17 @@
+/*
+ *
+ * Module: bibtex_messages
+ *
+ * This module contains the definition of several warnings, error, and progress reports
+ *
+ * Creator: Henderik A. Proper (erikproper@fastmail.com)
+ *
+ * Version of: 24.04.2024
+ *
+ */
+
+package main
+
+const (
+	WarningDBLPMismatch = "Found %s as dblp alias, while we already have %s for entry %s"
+)
